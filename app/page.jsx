eb52a6e6fbbfa15793da3528e11d8946dc5fa49b
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-06-18 17:10:26 +0530

    Next Auth and Navbar

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+    Embrace Pride,
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> Connect with Love and Equality.</span>
+    </h1>
+    <p className='desc text-center'>
+    PrideConnect aims to create a safe and welcoming space for the LGBTQ+ community and allies. 
+    </p>
+
+  
+  </section>
+);
+
+export default Home;
